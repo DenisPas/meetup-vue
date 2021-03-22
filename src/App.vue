4d7be608b5fd0c44a983a19e4f2c49b5703b1c2d
Denis_Pas <denis.pas2012@yandex.ru>
2021-03-22 20:40:52 +0300

    create project

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <v-toolbar>
+      <v-toolbar-title> Meetup-vue</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-toolbar-items>
+        <v-btn flat> Button</v-btn>
+      </v-toolbar-items>
+    </v-toolbar>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+};
+</script>
