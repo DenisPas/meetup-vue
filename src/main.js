43c467056c6f08116a50972df98eb3bf126f1978
Denis_Pas <denis.pas2012@yandex.ru>
2021-03-31 22:19:11 +0300

    add firebase

--- src/main.js
+++ src/main.js
@@ -3,9 +3,9 @@ import VueI18n from 'vue-i18n'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
-import {store} from './store'
+import { store } from './store'
 import i18n from './i18n'
-
+import * as firebase from 'firebase'
 
 
 Vue.use(VueI18n)
@@ -13,10 +13,21 @@ Vue.use(VueI18n)
 Vue.config.productionTip = false
 
 new Vue({
-  VueI18n,
-  vuetify,
-  router,
-  store,
-  i18n,
-  render: h => h(App)
+	VueI18n,
+	vuetify,
+	router,
+	store,
+	i18n,
+	render: h => h(App),
+	created() {
+		firebase.initializeApp({
+			apiKey: "AIzaSyDvMe3bZHGPk7rh6MQHN_RJC5dgTbzf5ZM",
+			authDomain: "meetup-app-354ca.firebaseapp.com",
+			projectId: "meetup-app-354ca",
+			storageBucket: "meetup-app-354ca.appspot.com",
+			messagingSenderId: "817928996467",
+			appId: "1:817928996467:web:57c9b21c35d67734170c06",
+			measurementId: "G-1JJ9Q4KDKP"
+		})
+	}
 }).$mount('#app')
